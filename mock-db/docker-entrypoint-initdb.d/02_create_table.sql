5f4c693c9e6dfc99e6fe0264c9755f8f75340d20
HoppingGanon <hoppingganonapp@gmail.com>
2023-07-23 18:20:44 +0900

    [change] 初期実行スクリプト変更

--- mock-db/docker-entrypoint-initdb.d/02_create_table.sql
+++ mock-db/docker-entrypoint-initdb.d/02_create_table.sql
@@ -3,15 +3,15 @@
 
 -- テーブル作成
 CREATE TABLE  memochema.test (
-  id NUMBER,
+  id INTEGER,
   name VARCHAR(10),
   value VARCHAR(10),
   PRIMARY KEY (id)
 );
 
 -- 権限追加
-GRANT ALL PRIVILEGES ON memochema.sample TO backend;
+GRANT ALL PRIVILEGES ON memochema.test TO backend;
 
 -- サンプルレコード作成
-INSERT INTO memochema.sample VALUES(1, 'ゴリラ', 'ぴんくだま');
-INSERT INTO memochema.sample VALUES(1, 'がのん', 'カズヤ');
+INSERT INTO memochema.test VALUES(1, 'ゴリラ', 'ぴんくだま');
+INSERT INTO memochema.test VALUES(2, 'がのん', 'カズヤ');
